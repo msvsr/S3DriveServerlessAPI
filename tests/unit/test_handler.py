14c85206598d865e8026fa4da2c38555899200bb
MSVSR <msvsr11297@gmail.com>
2022-02-25 18:04:07 +0530

    Added test cases for checking if they run at build

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -2,3 +2,22 @@ import json
 import lambdas.presigned_url.generate_presigned_url as gpu
 
 # Need to write unit test cases
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+
+
+if __name__ == '__main__':
+    unittest.main()
